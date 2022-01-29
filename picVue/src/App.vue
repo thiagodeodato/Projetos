8880859170523bf6a6f21423ec8ff3202b54f10a
Thiago Deodato <thiago-deodato@hotmail.com>
2022-01-29 12:35:03 -0300

    created pic list from api

--- picVue/src/App.vue
+++ picVue/src/App.vue
@@ -1,9 +1,16 @@
+
+
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="body">
+    <h1 class="centralized">{{ titulo }}</h1>
+    <ul class="pics-list">
+      <li class="pics-list-item" v-for="foto of fotos">
+        <div class="panel">
+          <h2 class="panel-title">{{ foto.titulo }}</h2>
+          <div class="panel-content">
+            <img class="responsive-image" :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -15,30 +22,70 @@ export default {
   data() {
     
     return {
-      titulo: 'Alugueis de Apartamento',
+      titulo: 'Galeria de Fotos',
       fotos: [
-        {
-          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
-          titulo: 'Apartamento 84m²'
-        },
-        {
-          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
-          titulo: 'Apartamento 84m²'
-        },
-        {
-          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
-          titulo: 'Apartamento 84m²'
-        },
-        {
-          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
-          titulo: 'Apartamento 84m²'
-        }
+        
       ]
     }
+  },
+
+  created() {
+
+    /*
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos);
+    */
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
   
 }
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralized {
+    text-align: center;
+  }
+
+  .pics-list {
+    list-style: none;
+  }
+
+  .pics-list-item {
+    display: inline-block;
+  }
+
+   .panel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .panel .panel-title {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  .responsive-image {
+    width: 100%
+  }
 </style>
