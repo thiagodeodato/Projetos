19a028080d834922ed10e51d4825dc81abf474c6
Thiago Deodato <thiago-deodato@hotmail.com>
2022-01-29 11:18:17 -0300

    Initializing vue project

---
+++ picVue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    
+    return {
+      titulo: 'Alugueis de Apartamento',
+      fotos: [
+        {
+          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
+          titulo: 'Apartamento 84m²'
+        },
+        {
+          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
+          titulo: 'Apartamento 84m²'
+        },
+        {
+          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
+          titulo: 'Apartamento 84m²'
+        },
+        {
+          url: 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
+          titulo: 'Apartamento 84m²'
+        }
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style>
+</style>
